d995f58b3a08315717c50bc6e501befebf6b9ba3
MostafaSabry55 <mostafasabry266@gmail.com>
2022-02-05 13:42:27 +0200

    Fixed recipes (7.4.4)

--- src/main/java/ms55/moreplates/common/data/ItemTags.java
+++ src/main/java/ms55/moreplates/common/data/ItemTags.java
@@ -26,42 +26,22 @@ public class ItemTags extends ItemTagsProvider {
     			limit = 2;
     		}
 
-    		/*for (int i = 0; i < Groups.metals.length; i++) {
-    			if (material == Groups.metals[i]) {
-    				limit = 3;
-    				break;
-    			} else {
-    				limit = 2;
-    			}
-    		}
-
-    		if (limit != 3) {
-    			if (material == EnumMaterials.GOLD || material == EnumMaterials.IRON) {
-    				limit = 3;
-    			} else {
-    				limit = 2;
-    			}
-    		}*/
-
     		for (int i = 0; i < limit; i++) {
     			if (i == 0) {
     				ResourceLocation PLATE = new ResourceLocation(MorePlates.MODID, material.toString() + "_plate");
         	        IOptionalNamedTag<Item> tag = Items.createTag("plates", material.toString());
-        	        getOrCreateBuilder(tag).addOptional(PLATE).replace();
-        	        getOrCreateBuilder(Items.PLATES).addOptional(PLATE).replace();
-        	        //getOrCreateBuilder(Items.PLATES).addTag(tag).replace();
+        	        getOrCreateBuilder(tag).addOptional(PLATE);
+        	        getOrCreateBuilder(Items.PLATES).addOptional(PLATE);
     			} else if (i == 1) {
     				ResourceLocation GEAR = new ResourceLocation(MorePlates.MODID, material.toString() + "_gear");
         	        IOptionalNamedTag<Item> tag = Items.createTag("gears", material.toString());
-        	        getOrCreateBuilder(tag).addOptional(GEAR).replace();
-        	        getOrCreateBuilder(Items.GEARS).addOptional(GEAR).replace();
-        	        //getOrCreateBuilder(Items.GEARS).addTag(tag).replace();
+        	        getOrCreateBuilder(tag).addOptional(GEAR);
+        	        getOrCreateBuilder(Items.GEARS).addOptional(GEAR);
     			} else {
     				ResourceLocation STICK = new ResourceLocation(MorePlates.MODID, material.toString() + "_stick");
         	        IOptionalNamedTag<Item> tag = Items.createTag("rods", material.toString());
-        	        getOrCreateBuilder(tag).addOptional(STICK).replace();
-        	        getOrCreateBuilder(Items.RODS).addOptional(STICK).replace();
-        	        //getOrCreateBuilder(Items.RODS).addTag(tag).replace();
+        	        getOrCreateBuilder(tag).addOptional(STICK);
+        	        getOrCreateBuilder(Items.RODS).addOptional(STICK);
     			}
     		}
 		}
